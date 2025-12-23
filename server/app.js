ad822c4c014a11af388e969729813b5e29dac347
Poseryaev <kirill.vova@mail.ru>
2025-12-23 18:14:24 +0400

    Add /

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 // ============================================
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   if (!req.path.startsWith('/api')) {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   }
